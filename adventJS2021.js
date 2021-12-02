fb8aa50652b513a1c38efe0cf680336b148c7807
Diego <44611837+seniorglez@users.noreply.github.com>
2021-12-02 19:59:55 +0000

    Update adventJS2021.js

--- adventJS2021.js
+++ adventJS2021.js
@@ -4,3 +4,15 @@ export default function contarOvejas(ovejas) {
   return ovejas.filter(o => o.color=="rojo")
   .filter(o => (/^.*(A.*N|N.*A).*$/i).test(o.name));
 }
+
+//challenge 2
+
+export default function listGifts(letter) {
+ return letter.trim()
+ .split(/\s+/)
+ .filter(word => !word.startsWith('_'))
+ .reduce((acc, cur)=> {
+     acc[cur] = (acc[cur] ?? 0) + 1;
+     return acc;
+ }, {})
+}
